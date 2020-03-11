8973ff3b9cbf52134ff0c789cb53cbc50b8e7737
b-hadri <b-hadri@users.noreply.github.com>
2020-03-11 08:51:28 +0100

    suppression des commentaires inutiles et amélioration des messages sysout

--- java3helb2eme/src/main/java/hbi/training/exercices/java3helb2eme/ex005Canard/UseCase001Abdel.java
+++ java3helb2eme/src/main/java/hbi/training/exercices/java3helb2eme/ex005Canard/UseCase001Abdel.java
@@ -7,26 +7,18 @@ public class UseCase001Abdel {
 
     public static void main(String[] args) {
 
-
-        // créer un Colvert ou un Fuligule
 //        Canard canard = FabriqueCanard.create("FULIGULE");
         Canard canard = FabriqueCanard.create("COLVERT");
 
-        // voler 10 j.
-        canard.voler(10);
+        canard.vole(10);
 
-        // se blesse
         canard.seBlesse();
 
-        // fait un cancan
         canard.cancanne();
 
-        // il guérit
         canard.guerit();
 
-        // fait un cancan
         canard.cancanne();
-
 
     }
 
