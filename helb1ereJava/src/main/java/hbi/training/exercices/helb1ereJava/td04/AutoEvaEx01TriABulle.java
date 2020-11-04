6fbed36d04b11fb956967041ad19b658977467c6
b-hadri <b-hadri@users.noreply.github.com>
2020-11-04 21:07:04 +0100

    ajoute le tri a bulle

--- helb1ereJava/src/main/java/hbi/training/exercices/helb1ereJava/td04/AutoEvaEx01TriABulle.java
+++ helb1ereJava/src/main/java/hbi/training/exercices/helb1ereJava/td04/AutoEvaEx01TriABulle.java
@@ -4,7 +4,7 @@ public class AutoEvaEx01TriABulle {
 
     public static void main(String[] args) {
 
-        int[] tableau = {2, 3, 0, 2, 1};
+        int[] tableau = {2, 3, 0, -1, 2, 1};
 
         afficherLeTableau(tableau);
         tableau = trierLeTableau(tableau);
