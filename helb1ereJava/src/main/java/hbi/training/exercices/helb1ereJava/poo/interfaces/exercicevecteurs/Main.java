c98f6ba9f6f16ca07969e83e8a7b76f5a84dbb46
b-hadri <b-hadri@users.noreply.github.com>
2021-04-25 13:39:00 +0200

    refactoriser l'exercice

--- helb1ereJava/src/main/java/hbi/training/exercices/helb1ereJava/poo/interfaces/exercicevecteurs/Main.java
+++ helb1ereJava/src/main/java/hbi/training/exercices/helb1ereJava/poo/interfaces/exercicevecteurs/Main.java
@@ -24,13 +24,15 @@ public class Main {
         IVecteur v1 = new Tableau(3);
         IVecteur v2 = new Tableau(3);
 
+        // v1(2,6,1)
         v1.affecterUneValeur(2, 0);
         v1.affecterUneValeur(6, 1);
         v1.affecterUneValeur(1, 2);
 
+        // v2(5,5,5)
         v2.affecterLaMemeValeurATousLesElements(5);
 
-        // ps = v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+        // produit scalaire = v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
 
         System.out.println(v1.produitScalaireAvec(v2));
 
